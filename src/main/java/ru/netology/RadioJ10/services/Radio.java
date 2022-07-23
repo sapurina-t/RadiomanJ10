d6a642ab7af79139331ae65b0876b6490541a2c1
taya sapurina <sapurina0t@gmail.com>
2022-07-23 20:22:45 +0500

    radio station branch100%

--- src/main/java/ru/netology/RadioJ10/services/Radio.java
+++ src/main/java/ru/netology/RadioJ10/services/Radio.java
@@ -2,7 +2,7 @@ package ru.netology.RadioJ10.services;
 
 public class Radio {
     private int currentVolume;
-//    public int currentStationNumber;
+    private int currentStationNumber;
 
     public int getCurrentVolume() {
         return currentVolume;
@@ -31,6 +31,36 @@ public class Radio {
             currentVolume--;
         } else {
             currentVolume = 0;
+        }
+    }
+
+    public int getCurrentStationNumber() {
+        return currentStationNumber;
+    }
+
+    public void setCurrentStationNumber(int currentStationNumber) {
+        if (currentStationNumber > 9) {
+            return;
+        }
+        if (currentStationNumber < 0) {
+            return;
+        }
+        this.currentStationNumber = currentStationNumber;
+    }
+
+    public void next() {
+        if (currentStationNumber < 9) {
+            currentStationNumber++;
+        } else {
+            currentStationNumber = 0;
+        }
+    }
+
+    public void prev() {
+        if (currentStationNumber > 0) {
+            currentStationNumber--;
+        } else {
+            currentStationNumber = 9;
         }
     }
 }
--- src/test/java/ru/netology/RadioJ10/services/RadioTest.java
+++ src/test/java/ru/netology/RadioJ10/services/RadioTest.java
@@ -13,7 +13,7 @@ public class RadioTest {
         int actual = radio.getCurrentVolume();
 
         Assertions.assertEquals(expected, actual);
-}
+    }
 
     @Test
     public void shouldIncreaseVolumeMax() {
@@ -78,4 +78,79 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldSwitchToTheNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(8);
+
+        radio.next();
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToZeroStationNext() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(9);
+
+        radio.next();
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToThePreviousStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(1);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSwitchToZeroStationPrev() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(0);
+
+        radio.prev();
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBeNoMoreLastStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(10);
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mustNotBeLessThanTheFirstStation() {
+        Radio radio = new Radio();
+        radio.setCurrentStationNumber(-1);
+
+        int actual = radio.getCurrentStationNumber();
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
